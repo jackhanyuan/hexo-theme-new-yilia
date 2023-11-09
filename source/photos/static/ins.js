b7208052a7f2bb7f57d2a07654fd12ed149a5702
jackhanyuan <jackhanyuan@foxmail.com>
2023-11-09 19:42:07 +0800

    fix path error https://github.com/jackhanyuan/hexo-theme-new-yilia/issues/10

--- source/photos/static/ins.js
+++ source/photos/static/ins.js
@@ -124,7 +124,7 @@
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="./static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
